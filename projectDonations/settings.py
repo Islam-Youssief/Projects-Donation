da7d5baaac0f91f7ed9e7528b835c8d1203b1abd
Islam Youssief <islam.youssief@gmail.com>
2019-05-06 16:45:18 +0200

    Implementing projects model

--- projectDonations/settings.py
+++ projectDonations/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'projects',
+    'taggit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from taggit.managers import TaggableManager
+import uuid
+import os
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=50)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    details = models.TextField()
+    target = models.IntegerField()
+    tags = TaggableManager()
+    is_featured = models.BooleanField(default=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.project_name
+
+
+class Donation(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.amount
+
+
+class ProjectRate(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    rate = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.user + ' rated ' + self.project + ' with rate : ' + self.rate
+
+
+class ProjectPictures(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='photos/')
+
+    def __str__(self):
+        return self.project.title + ' has a new picture .'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.user + ' Comment ' + self.project.title + ' with comment : ' + self.comment
+
+
+class Reply(models.Model):
+    reply = models.TextField()
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reply + ' On Comment ' + self.comment + ' with user : ' + self.user
+
+
+class ReportedProject(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    is_reported = models.BooleanField()
+
+
+class ReportedComment(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    is_reported = models.BooleanField()
+
+
+class ReportedReply(models.Model):
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    is_reported = models.BooleanField()
