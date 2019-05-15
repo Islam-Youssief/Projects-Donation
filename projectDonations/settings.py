845f27919239b1b07ae70d5c24ecb4dbbde52744
melgohary95 <mfelfel95@gmail.com>
2019-05-15 23:48:12 +0200

    merging some conflicts

--- projectDonations/settings.py
+++ projectDonations/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbcrowd',
-        'USER': 'fatema',
-        'PASSWORD': '1234',
+        'USER': 'root',
+        'PASSWORD': 'felfel',
         'HOST': 'localhost',
         'PORT': '3306',
     }
