9b011306eeb883604cb1a2d383857271af15a1ee
Islam Youssief Mohamed <Islam.youssief@gmail.com>
2019-05-16 17:46:03 +0200

    Setting up the user model and users urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
-
+from django.urls import path, re_path
+from .views import *
+app_name = "users"
 urlpatterns = [
-    # path('', index),
+    path("login", loginuser, name="login"),
+    path("logout", logout_view, name="logout"),
+    path("profile/<int:uid>", profile, name="profile"),
+    path("register", signup_new_user, name="register"),
+    path("edit/<int:uid>", editprofile, name="edit"),
+    path("delete", deleteAccount, name="delete"),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            activate, name='activate')
 ]
